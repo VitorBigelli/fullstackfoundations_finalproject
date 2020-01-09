973c5058e80f9a7d0f3805c0fccb1993973bede5
vitorbigelli <vitorbigelli@hotmail.com>
2020-01-09 13:06:29 -0300

    Add edit restaurant funcionality

--- webserver.py
+++ webserver.py
@@ -14,18 +14,6 @@ session = DBSession()
 
 app = Flask(__name__) 
 
-
-#Fake Restaurants
-restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
-
-
-#Fake Menu Items
-items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
-
-
 # Show all restaurants
 @app.route('/') 
 @app.route('/restaurants') 
@@ -49,9 +37,18 @@ def newRestaurant():
 
 # Edit a restaurant
 @app.route('/restaurants/<int:restaurant_id>/edit', methods=['GET', 'POST']) 
-def editRestaurant(restaurant_id): 
-    return render_template('editRestaurant.html', restaurant_id = restaurant.id )
+def editRestaurant(restaurant_id):
 
+    restaurantToEdit = session.query(Restaurant).filter_by( id = restaurant_id).one()
+    if request.method == 'GET': 
+        return render_template('editRestaurant.html', restaurant = restaurantToEdit )
+    else: 
+        if request.form['name']: 
+            restaurantToEdit.name = request.form['name']
+        session.add(restaurantToEdit)
+        session.commit()
+        return redirect(url_for('showRestaurants'))
+    
 # Delete a restaurant
 @app.route('/restaurants/<int:restaurant_id>/delete', methods=['GET', 'POST']) 
 def deleteRestaurant(restaurant_id): 
