83593b86e6e5c2748031e7537fe3253b54968f43
vitorbigelli <vitorbigelli@hotmail.com>
2020-01-09 11:48:41 -0300

    Create menu.html template

--- webserver.py
+++ webserver.py
@@ -40,12 +40,12 @@ def newRestaurant():
 # Edit a restaurant
 @app.route('/restaurants/<int:restaurant_id>/edit', methods=['GET', 'POST']) 
 def editRestaurant(restaurant_id): 
-    return render_template('editRestaurant.html', restaurant_id = restaurant_id )
+    return render_template('editRestaurant.html', restaurant_id = restaurant.id )
 
 # Delete a restaurant
 @app.route('/restaurants/<int:restaurant_id>/delete', methods=['GET', 'POST']) 
 def deleteRestaurant(restaurant_id): 
-    return render_template('deleteRestaurant.html', restaurant_id = restaurant_id )
+    return render_template('deleteRestaurant.html', restaurant_id = restaurant.id )
 
 # -------------------------------------------------------------------------------------------------------------
 
@@ -53,22 +53,22 @@ def deleteRestaurant(restaurant_id):
 @app.route('/restaurant/<int:restaurant_id>')
 @app.route('/restaurant/<int:restaurant_id>/menu') 
 def showMenu(restaurant_id): 
-    return '<html><body> This page will show menu for restaurant %s </body></html>' % restaurant_id 
+    return render_template('menu.html', restaurant = restaurant, items = items )
 
 # Create a new menu item 
 @app.route('/restaurant/<int:restaurant_id>/menu/new', methods=['GET', 'POST'])
 def newMenuItem(restaurant_id): 
-    return '<html><body> This page will be for making a new menu item for restaurant %s </body></html>' % restaurant_id 
+    return '<html><body> This page will be for making a new menu item for restaurant %s </body></html>' % restaurant.id 
 
 # Edit a menu item
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menuitem_id>/edit', methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menuitem_id): 
-    return '<html><body> This page will be for editing menu item %s </body></html>' % menuitem_id 
+    return '<html><body> This page will be for editing menu item %s </body></html>' % item.id 
 
 # Delete a menu item 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menuitem_id>/delete', methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id, menuitem_id): 
-    return '<html><body> This page will be for deleting menu item %s </body></html>' % menu_menuitem_iditem 
+    return '<html><body> This page will be for deleting menu item %s </body></html>' % item.id 
 
 
 
